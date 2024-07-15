9b081d89bd8f430ab4ed97c7be64c1e15df1d374
llayz46 <layzer@outlook.fr>
2024-07-15 20:45:14 +0200

    Adding favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $pageTitle }} - {{ config('app.name') }}</title>
+    <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
     @vite('resources/css/app.css')
 </head>
 <body class="h-full bg-background transition-colors" data-darkmode-target="body" data-controller="darkmode">
